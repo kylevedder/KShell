1dd093d25efac7eb6cef17f5d246b886c86e72cf
Kyle <Kyle@192.168.1.36>
2014-10-27 20:48:35 -0400

    More Work Done.

--- src/kshell/CommandHolder.java
+++ src/kshell/CommandHolder.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package kshell;
 
 import java.util.ArrayList;
@@ -16,22 +15,90 @@ import java.util.List;
  */
 public class CommandHolder
 {
+
     private List<Command> list;
-    
+
     public CommandHolder()
     {
         list = Collections.synchronizedList(new ArrayList<Command>());
     }
-    
-    
+
     /**
-     * Add an Command to the 
-     * @param cmd 
+     * Add an Command to the CommandHolder.
+     *
+     * @param cmd
      */
     public void addCommand(Command cmd)
     {
         list.add(cmd);
     }
-    
-    
+
+    /**
+     * Add a given String to the CommandHolder.
+     *
+     * @param s
+     */
+    public void addCommand(String s)
+    {
+//        Command cmd = new Command
+
+    }
+
+    private Command parseIntoCommand(String s)
+    {
+        s = s.trim();
+        if(!s.contains(" "))//simply a command
+        {
+            return new Command(s, null, null);
+        }
+        else
+        {            
+            int firstSpace = s.indexOf(" ");
+            String command = s.substring(0, firstSpace);
+            String beyondFirstSpace = s.substring(firstSpace).trim();
+            if(beyondFirstSpace.startsWith("-"))//has arguments
+            {
+                int nextSpace = beyondFirstSpace.indexOf(" ");
+                if(nextSpace < 0)//just the command and args, no space after exists
+                {                    
+                    return new Command(command, beyondFirstSpace, null);
+                }                
+                else//actually exists, has command, args, and param
+                {
+                    String args = beyondFirstSpace.substring(0, nextSpace);
+                    String param = s.substring(nextSpace).trim();
+                    return new Command(command, args, param);
+                }
+                
+            }
+            else//no args
+            {
+                return new Command(command, null, beyondFirstSpace);
+            }
+            
+        }
+//        int commandSplit = s.indexOf(" ");
+//        String command = s.substring(0, commandSplit);//splits the string at the first space. This is the command string.
+//        String args = null;
+//        String param = null;
+//        if (commandSplit < s.length() - 1)//is more than just a single command
+//        {
+//            if(s.substring(commandSplit).trim().startsWith("-"))//is an argument
+//            {
+//                
+//            }
+//            
+//            
+//            if (s.substring(commandSplit).startsWith("\""))//starts with quote
+//            {
+//                int indexOfCloseQuote = s.indexOf(s, commandSplit + 1);
+//                if(indexOfCloseQuote>=commandSplit)//close quote exists
+//                {
+//                    
+//                }
+//            }
+//        }
+//        String String[] split = s.split(" ");
+    }
+
 }
